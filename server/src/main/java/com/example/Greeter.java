a5f791d881f3be4ee11f67b479fc3ef01efda229
mouloud-hamdidouche@trade <mouloud-hamdidouche@trade.gov>
2019-12-27 17:10:57 -0500

    test freestyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,12 +9,14 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-
+	  
   }
-	/**
-	 * This read excel file into arraylist with filters
-	 * @param someone: someone 
-	 */
+  
+  /**
+	* This read excel file into arraylist with filters
+	* @param someone is the name of a persomeone
+	* @return geeting string
+	*/
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
